c13bcb61b184d47000b43f2a049584d4dc985639
bricetangang <brice.tangang@dembou-consulting.com>
2022-06-21 23:45:58 +0100

    Create Beer Controller. Closes #3

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerPageList.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPageList.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public class BeerPageList  extends PageImpl<BeerDto> {
-    
+
     public BeerPageList(List<BeerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
     }
