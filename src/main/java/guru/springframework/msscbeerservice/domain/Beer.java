2e4ba03d8e35e540b693391246c72b0ef402c7c3
bricetangang <brice.tangang@dembou-consulting.com>
2022-06-24 03:14:58 +0100

    Create Beer JPA Entity. Closes #5

---
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -0,0 +1,42 @@
+package guru.springframework.msscbeerservice.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    @Version
+    private Long version;
+
+    private String beerName;
+    private String beerStyle;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+
+    private Long quantityOnHand;
+    private Long quantityToBrew;
+}
