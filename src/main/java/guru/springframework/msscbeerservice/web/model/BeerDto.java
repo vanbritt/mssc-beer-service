969e709f65622ba786cf3213c50a0df39da33481
bricetangang <brice.tangang@dembou-consulting.com>
2022-06-28 23:49:35 +0100

    Added Bean Validation. Closes #7

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,15 +18,28 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID id;
+    @Null
     private  Integer version;
+    @NotNull
     private BeerStyleEnum beerStyle;
+    @Positive
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
+
+    @NotBlank
     private String beerName;
+
     private Integer quantityOnHand;
 
+    @Null
     private OffsetDateTime lastModifiedDate;
+    @Null
     private OffsetDateTime createdDate;
 
 
